7172becef30bb434f2e33ac190679e4bf5f5febf
Ashish-Prakash <ashishprakash52433@gmail.com>
2022-06-14 21:04:54 +0530

    Express Init

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+let port = "8080";
+app.listen(port, function () {
+  console.log("Server is listen to port 8080");
+});
+app.use(express.json());
+let user = {};
+app.get("/", (request, response) => {
+  response.send("<h1>Hello</h1>");
+});
+app.get("/home", (req, res) => {
+  res.sendFile("./views/index.html", { root: __dirname });
+});
+
+app.get("/user", (req, res) => {
+  res.json(user);
+});
+
+app.post("/user", (req, res) => {
+  user=req.body;
+  res.send('data has been added succesfully');
+});
+
+app.patch("/user", (req, res) => {
+  let obj = req.body;
+  for(let key in obj){
+    user[key] = obj[key];
+  }
+  res.json(user);
+});
+
+app.delete("/user", (req, res) => {
+  user = {};
+  res.json(user);
+});
